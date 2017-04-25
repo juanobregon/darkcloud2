b4c4525dc950cf078737924581d9b0a8c829e8a3
Juan Obregon <juanobregon26@gmail.com>
2017-04-25 01:18:02 -0400

    Updated the monster HTML to fit more monsters

--- application/views/home.php
+++ application/views/home.php
@@ -1,4 +1,5 @@
-<html DOCTYPE!>
+<!DOCTYPE html>
+<html>
 <head>
     <title>Dark Cloud 2</title>
     <!-- Latest compiled and minified CSS -->
@@ -12,19 +13,25 @@
 </head>
 <body>
     <div class="container">
-        <section>
-            <div class="page-header">
-                <h1>Enemies</h1>
-            </div>
-            <img data-src="holder.js/200x200" class="img-thumbnail" alt="200x200" style="width: 200px; height: 200px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgdmlld0JveD0iMCAwIDIwMCAyMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzIwMHgyMDAKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWJhMzBkMzk3YyB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1YmEzMGQzOTdjIj48cmVjdCB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9Ijc0LjA2MjUiIHk9IjEwNC41NTYyNSI+MjAweDIwMDwvdGV4dD48L2c+PC9nPjwvc3ZnPg==" data-holder-rendered="true">
-            <img src="assets/images/thumbs/bat.png" alt="" width="200">
-            <div class="caption">
-            	<h3><?php echo $bat->getName(); ?></h3>
-            </div>
-        </section>
-        <section>
+        <div class="page-header">
+            <h1>Enemies</h1>
+        </div>
+        <div class="row">
 
-        </section>
+            <!-- Start Monster -->
+            <div class="col-md-2">
+                <div class="thumbnail">
+                    <a href="javascript:">
+                        <img src="assets/images/thumbs/bat.png" alt="">
+                        <div class="caption text-center">
+                            <h5><?php echo $bat->getName(); ?></h5>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <!-- End Monster -->
+            
+        </div>
     </div>
 </body>
 </html>
