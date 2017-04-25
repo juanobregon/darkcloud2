09ce03631315aa403acf9e68b1b5707bad4b0b2b
Juan Obregon <juanobregon26@gmail.com>
2017-04-25 00:50:34 -0400

    Added phpdoc to the Bat class

--- application/models/Bat.php
+++ application/models/Bat.php
@@ -2,15 +2,53 @@
 
 include 'Monster.php';
 
+/**
+ * Class Bat
+ */
 class Bat extends Monster
 {
+	/**
+	 * @var string
+	 */
 	protected $name = 'Bat';
+
+	/**
+	 * @var string
+	 */
 	protected $type = 'Beast';
+
+	/**
+	 * @var string
+	 */
 	protected $habitat = 'Underground Channel';
+
+	/**
+	 * @var int
+	 */
 	protected $abs = 6;
+
+	/**
+	 * @var int
+	 */
 	protected $gilda = 6;
+
+	/**
+	 * @var array
+	 */
 	protected $weaknesses = array('Cyclone');
+
+	/**
+	 * @var array
+	 */
 	protected $effective_weapons = array('Gun', 'Beam', 'Magic');
+
+	/**
+	 * @var array
+	 */
 	protected $ineffective_weapons = array('Wrench', 'Grenade');
+
+	/**
+	 * @var array
+	 */
 	protected $items_dropped = array('Holy Water', 'Wind Crystal', 'Bundle of Hay');
 }
